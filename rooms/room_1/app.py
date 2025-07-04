ac4f437820411b70c38a6fd7d0568809bf99b232
ZY <yuhang.zhu@studio.unibo.it>
2025-07-04 19:49:28 +0200

    Add first room

---
+++ rooms/room_1/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.rout("/home", methods=["GET"])
+def start():
+    return jsonify({"status": "ok", "message": f"Inizio puzzle"})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
