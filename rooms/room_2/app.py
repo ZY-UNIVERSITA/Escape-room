a37a8dfa8747c55dd06705a8d4f893821303b52d
ZY <yuhang.zhu@studio.unibo.it>
2025-07-04 22:38:39 +0200

    Add 2° rooms

---
+++ rooms/room_2/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+SECRET_CODE = "8271"
+puzzle_solved = False
+
+@app.route("/", methods=["GET"])
+def start():
+    return jsonify({"status": "ok"})
+
+@app.route('/answer', methods=['POST'])
+def puzzle():
+    return
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5002)
