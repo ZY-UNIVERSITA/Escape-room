c29c480ff42df647a2b568c850b94ab41d263cf0
ZY <yuhang.zhu@studio.unibo.it>
2025-07-05 16:26:42 +0200

    Fix logger

--- game_controller/app.py
+++ game_controller/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
+import logging
+import os
 
 app = Flask(__name__)
+
+app.logger.setLevel(logging.INFO)
 
 PUZZLE_ROOM_URL = {
     "1": "http://puzzle_room_1:5001",
@@ -12,31 +16,46 @@ current_room: int = 1
 game_state = {
     "solved_rooms": [],
     "inventory": {
-
     }
 }
 
 @app.route("/", methods=["GET"])
+def home():
+    return app.send_static_file("index.html")
+
+@app.route("/start", methods=["GET"])
 def start():
+    global current_room
+
     response = requests.get(f"{PUZZLE_ROOM_URL[str(current_room)]}/")
     data = response.json()
 
     return jsonify(data)
 
+
 @app.route("/answer", methods=["POST"])
 def answer():
+    global current_room
+
     action = request.json.get("answer")
+    
+    app.logger.info(f"Risposta utente: {action}")
 
-    response = requests.post(f"{PUZZLE_ROOM_URL[str(current_room)]/answer}", json={"answer": action})
-    data = response.json()
+    response = requests.post(f"{PUZZLE_ROOM_URL[str(current_room)]}/answer", json={"answer": action})
 
-    if (response.json.get("status")) == "ok":
+    try:
+        data = response.json()
+    except ValueError:
+        data = {}
+
+    if (data.get("status")) == "ok":
         if data.get("object"):
             game_state["inventory"][current_room] = data.get("object")
 
         game_state["solved_rooms"].append(current_room)
 
         current_room += 1
+
 
     return jsonify(data)
 
