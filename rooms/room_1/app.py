580a41f55fa60c4794caba449d22ef25e57d964d
ZY <yuhang.zhu@studio.unibo.it>
2025-07-05 16:26:21 +0200

    Add more input validation

--- rooms/room_1/app.py
+++ rooms/room_1/app.py
@@ -1,38 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
+import logging
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
 __equation: str = "x + 4 = 168"
-__answer: int = 4114
+CORRECT_ANSWER: int = 41
 
-__secret_code: int = 1
+SECRET_CODE: int = 1
 
 @app.route("/", methods=["GET"])
 def start():
     message = {
         "status": "ok",
         "message": f"Per poter proseguire nella prossima stanza, devi risolvere la seguente equazione che dovrai specchiare e il codice segreto ti sarà rivelato.",
-        "puzzle": __equation
+        "puzzle": __equation,
     }
 
     return jsonify(message)
 
-
 @app.route("/answer", methods=["POST"])
 def answer():
-    answer: int = request.json.get("answer")
+    if not request.json:
+        abort(415, description="La richiesta di tipo POST non era un JSON.")
 
-    message = {}
+    data = request.get_json()
+    if "answer" not in data:
+        abort(400, description="Il campo 'answer' non è presente.")
 
-    if answer != __answer:
-        message["status"] = "ok"
-        message["message"] = f"La tua risposta è corretta. Hai ricevuto un oggetto, il numero: ${__secret_code}",
-        message["object"] =  __secret_code
+    try:
+        answer: int = int(data.get("answer"))
+    except ValueError:
+        abort(400, description="Il campo 'answer' non è un numero.")
+
+    app.logger.info(f"Risposta utente: {answer}")
+
+    if answer == CORRECT_ANSWER:
+        message = {
+            "status": "ok",
+            "message": f"La tua risposta è corretta. Hai ricevuto un oggetto, il numero: ${SECRET_CODE}",
+            "object": SECRET_CODE,
+        }
     else:
-        message["status"] = "wrong"
-        message["message"] = f"La tua risposta è sbagliata. Riprova."
+        message = {"status": "wrong", "message": "Risposta sbagliata, riprova"}
 
     return jsonify(message)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
