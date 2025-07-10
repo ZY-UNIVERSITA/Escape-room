9a0b57e272de4c299540c97210b2522074ff269a
ZY <yuhang.zhu@studio.unibo.it>
2025-07-10 19:54:45 +0200

    Use wekzeug in development mode

--- game_engine/app.py
+++ game_engine/app.py
@@ -16,4 +16,4 @@ def start():
     return jsonify({"status": "Connected to Game Engine"})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001)
+    socketio.run(app, host='0.0.0.0', port=5001, allow_unsafe_werkzeug=True)
